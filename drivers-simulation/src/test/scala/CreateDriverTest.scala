9998e9170fcb6ce3106ba5d24736452bb4378012
Andre Lucas <andrelucastic@gmail.com>
2024-01-03 22:49:30 +0000

    improving test

--- drivers-simulation/src/test/scala/CreateDriverTest.scala
+++ drivers-simulation/src/test/scala/CreateDriverTest.scala
@@ -1,8 +1,8 @@
 package io.andrelucas
 
-import business.driver.{CreateDriver, DriverDTO}
-import repository.DriverRepositoryInMemory
 
+import io.andrelucas.business.driver.{CreateDriver, DriverDTO}
+import io.andrelucas.repository.DriverRepositoryInMemory
 import org.scalatest.flatspec.AnyFlatSpec
 
 class CreateDriverTest extends AnyFlatSpec {
@@ -13,6 +13,14 @@ class CreateDriverTest extends AnyFlatSpec {
     createDriver.execute(DriverDTO("Andre", "Moto", "Recife"))
 
     assert(repository.findAll().nonEmpty)
+
+    val driversName = repository.findAll().map(d=> d.getName)
+    val driversModal = repository.findAll().map(d => d.getModal)
+    val driversDeliveryRegion = repository.findAll().map(d => d.getDeliveryRegion)
+
+    assert(driversName.contains("Andre"))
+    assert(driversModal.contains("Moto"))
+    assert(driversDeliveryRegion.contains("Recife"))
 
   }
 }
